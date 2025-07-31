28124bf94242c460abd0fca1fc02ebf71210854f
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-07-31 18:39:21 +0530

    code added

---
+++ voting.py
@@ -0,0 +1,83 @@
+import psycopg2
+from confluent_kafka import Consumer, KafkaException, KafkaError, SerializingProducer
+import simplejson as json
+import random
+from datetime import datetime
+
+from main import delivery_report
+
+config = {
+    'bootstrap.servers': 'localhost:9092'
+}
+
+consumer = Consumer(config | {
+    'group.id': 'voting-group',
+    'auto.offset.reset': 'earliest',
+    'enable.auto.commit': False
+})
+
+producer = SerializingProducer(config)
+
+if __name__ == "__main__":
+    connection = psycopg2.connect("host=localhost dbname=voting user=postgres password=postgres")
+    cursor = connection.cursor()
+
+    candidates_query = cursor.execute(
+        """
+            SELECT row_to_json(col)
+            FROM (
+                SELECT * FROM candidates
+                ) col;
+        """
+    )
+
+    candidates = [candidate[0] for candidate in cursor.fetchall()]
+    
+    if len(candidates) == 0:
+        raise Exception("No candidates found in the database")
+    else:
+        print(candidates)
+
+    consumer.subscribe(['voters_topic'])
+
+    try:
+        while True:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+            elif msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
+            else:
+                voter = json.loads(msg.value().decode('utf-8'))
+                chosen_candidate = random.choice(candidates)
+                vote = voter | chosen_candidate | {
+                    'voting_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    'vote': 1
+                }
+
+                try:
+                    print('User {} is voting for candidate {}'.format(vote['voter_id'], vote['candidate_id']))
+                    cursor.execute("""
+                        INSERT INTO votes(voter_id, candidate_id, voting_time)
+                        VALUES (%s, %s, %s)
+                    """,
+                        (
+                            vote['voter_id'], vote['candidate_id'], vote['voting_time']
+                        ))
+                    connection.commit()
+
+                    producer.produce(
+                        topic='votes_topic',
+                        key=vote['voter_id'],
+                        value=json.dumps(vote),
+                        on_delivery=delivery_report
+                    )
+
+                except Exception as e:
+                    print("Error: ", e)
+    except Exception as e:
+        print(e)
