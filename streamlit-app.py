6d93d963ee9742e791a80dea5fe13efcb2dbacd5
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-08-01 15:00:55 +0530

    code added

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,6 +3,7 @@ import time
 import psycopg2
 from kafka.consumer import KafkaConsumer
 import simplejson as json
+import pandas as pd
 
 @st.cache_data
 def fetch_voting_stats():
@@ -34,8 +35,14 @@ def create_kafka_consumer(topic_name):
     return consumer
 
 def fetch_data_from_kafka(consumer):
-    # messages = consumer.poll(timeout_ms=1000)
-    pass
+    messages = consumer.poll(timeout_ms=1000)
+    data = []
+
+    for message in messages.values():
+        for sub_message in message:
+            data.append(sub_message.value)
+
+    return data
 
 def update_data():
     last_refresh = st.empty()
@@ -46,6 +53,24 @@ def update_data():
     col1, col2 = st.columns(2)
     col1.metric("Total Voters", voters_count)
     col2.metric("Total Candidates", candidates_count)
+
+    consumer = create_kafka_consumer("aggregated_votes_per_candidate")
+    data = fetch_data_from_kafka(consumer)
+    results = pd.DataFrame(data)
+
+    results = results.loc[results.groupby('candidate_id')['total_votes'].idxmax()]
+    leading_candidate = results.loc[results['total_votes'].idxmax()]
+
+    st.markdown("""---""")
+    st.header("Leading Candidate")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(leading_candidate['photo_url'], width=200)
+    with col2:
+        st.header(leading_candidate['candidate_name'])
+        st.subheader(leading_candidate['party_affiliation'])
+        st.subheader(f"Total Vote: {leading_candidate['total_votes']}")
+
 
 if __name__ == "__main__":
     st.title("Realtime Election Voting Dashboard")
